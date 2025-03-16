f7d8cf8fd36c508d7808e7c5d9237c3b127f973e
Ömer Memes <omer.memes20@gmail.com>
2025-03-16 12:00:00 +0300

    Return Code

--- DATE and TIME/part1.bat
+++ DATE and TIME/part1.bat
@@ -5,3 +5,10 @@ echo %TIME%
 echo %DATE%
 
 pause > nul
+
+Error Code	Description
+0			Program successfully completed.
+1			Incorrect function. Indicates that Action has attempted to execute non-recognized command in Windows command prompt cmd.exe.
+2			The system cannot find the file specified. Indicates that the file cannot be found in specified location.
+3			The system cannot find the path specified. Indicates that the specified path cannot be found.
+5			Access is denied. Indicates that user has no access right to specified resource.
