195b4ccd3b81fe18d18782b657ce9d0dc8c0299d
Ömer Memes <omer.memes20@gmail.com>
2025-03-18 16:08:31 +0300

    NETWORK

---
+++ MIX/mix.bat
@@ -0,0 +1,17 @@
+
+:: NETWORK
+İşlem												Komut
+Mevcut kullanıcı hesaplarını listeleme				NET USER
+Yeni kullanıcı oluşturma							NET USER Omer Memes123 /ADD
+Kullanıcıyı silme									NET USER Omer /DELETE
+Ağ hizmetlerini listeleme							NET START
+Bir hizmeti başlatma								NET START "Print Spooler"
+Bir hizmeti durdurma								NET STOP "Print Spooler"
+Mevcut ağ bağlantılarını listeleme					NET USE
+Ağ sürücüsü bağlama									NET USE Z: \\192.168.1.100\OrtakDosya
+Ağ sürücüsü kaldırma								NET USE Z: /DELETE
+Bilgisayarın ağ yapılandırmasını gösterme			NET CONFIG
+Ağ istatistiklerini görüntüleme						NET STATISTICS WORKSTATION
+
+
+
